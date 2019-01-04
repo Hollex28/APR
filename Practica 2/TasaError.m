2f0d3a4632d009f545bca022620ab6a37c34bac2
TENDERO LARA <adtenla@EVIRL-027-OK.dsic.cloud>
2019-01-04 21:20:54 +0100

    terminado Splinker y empezado ejercicio entregable TasaError.m

---
+++ Practica 2/TasaError.m
@@ -0,0 +1,46 @@
+function terror = TasaError(fe,fle,fp,flp,numGaus)
+	addpath('~/asigDSIC/ETSINF/apr/p2/BNT')
+	addpath(genpathKPM('~/asigDSIC/ETSINF/apr/p2/BNT'))
+	
+	datApr = load(fe, '-ascii');
+	etqApr = load(fle, '-ascii');
+	
+	dataApr = zscore(datApr);
+	etiqApr = etqApr + 1;
+	[numVec dim] = size(dataApr)
+	numClas = max(etiqApr)
+
+	grafo = [ 0 1 1 ; 0 0 1 ; 0 0 0 ];
+	numNodos = length(grafo)
+	tallaNodos = [numClas numGaus dim];
+	nodosDiscretos = [1 2];
+	redB = mk_bnet(grafo, tallaNodos, 'discrete', nodosDiscretos);
+	redB.CPD{1} = tabular_CPD(redB, 1);
+	redB.CPD{2} = tabular_CPD(redB, 2);
+	redB.CPD{3} = gaussian_CPD(redB, 3, 'cov_type', 'diag');
+
+	datosApr = cell(numNodos, numVec);
+	datosApr(numNodos,:) = num2cell(dataApr', 1);
+	datosApr(1,:) = num2cell(etiqApr', 1);
+	motor = jtree_inf_engine(redB);
+	maxIter = 16;
+	[redB2, ll, motor2] = learn_params_em(motor, datosApr, maxIter);
+
+	datTest = load(fp, '-ascii');
+	etqTest = load(fpl, '-ascii');
+	dataTest = zscore(datTest);
+	etiqTest = etqTest + 1;
+	testeo = 0
+
+	p = zeros(length(dataTest), numClas);
+	evidencia = cell(numNodos,1);
+	for i=1:length(dataTest)
+		evidencia{numNodos} = dataTest(i,:)';
+		[motor3, ll] = enter_evidence(motor2, evidencia);
+		m = marginal_nodes(motor3, 1);
+		p(i,:) = m.T';
+		testeo = testeo +1
+	end
+	
+    
+end
